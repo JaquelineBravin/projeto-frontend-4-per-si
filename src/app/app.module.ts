a2a7082274eeeee28ef4eff1b215600926574b4c
Jaqueline Bravin <jaqbravin@hotmail.com>
2023-10-04 21:37:49 -0400

    Adicionando componente LIST

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { ProductsComponent } from './components/products/products.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductsComponent, HeaderComponent],
+  declarations: [AppComponent, ProductsComponent, HeaderComponent, ListComponent],
   imports: [BrowserModule, AppRoutingModule, MatToolbarModule, MatTableModule],
   providers: [],
   bootstrap: [AppComponent],
